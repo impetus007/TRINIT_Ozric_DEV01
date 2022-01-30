4bde1d05096e2fa179b66c1f8c2d23b376ce3d8b
vishal singh <76548939+impetus007@users.noreply.github.com>
2022-01-30 08:26:09 +0530

    Update Project.js

--- wix/src/components/project/Project.js
+++ wix/src/components/project/Project.js
@@ -3,11 +3,13 @@ import React from "react";
 import {Box} from '@material-ui/core'
 
 import Navbar from "../home/Navbar";
+import Grapes from ".../component/Grapes.js"
 
 function Project(){
     return(
        <Box>
            <Navbar/>
+           <Grapes />
        </Box>
     )
 }
